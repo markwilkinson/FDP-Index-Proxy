6c8f825886d970e75b2510ac6b7694a8ef9ab016
Mark Wilkinson <markw@illuminae.com>
2024-10-16 09:32:25 +0200

    initialize

---
+++ fdp_index_proxy/lib/fdp_index_proxy.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "fdp_index_proxy/version"
+
+module FdpIndexProxy
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ fdp_index_proxy/lib/fdp_index_proxy/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module FdpIndexProxy
+  VERSION = "0.1.0"
+end
